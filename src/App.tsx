785b46d9553ddf01bcade4235f561e37a8c8730a
DaroMacs <dariomacedog@gmail.com>
2023-05-01 20:45:23 -0600

    Adding functions and states to zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { shallow } from "zustand/shallow";
+import { useCounterStore } from "./store/counterStore";
+import { useEffect } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const { counter, title, posts } = useCounterStore(
+    (state) => ({
+      counter: state.count,
+      title: state.title,
+      posts: state.posts,
+    }),
+    shallow
+  );
+
+  const { increment, getPosts, clearStore, multiply } = useCounterStore();
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const handleIncrement = (value: number) => {
+    increment(value);
+  };
+
+  return (
+    <div>
+      <h1>{title + counter}</h1>
+      <button onClick={() => handleIncrement(30)}>Increment</button>
+      <button onClick={() => multiply(2)}>Multiply</button>
+      <button onClick={clearStore}>Clear Store</button>
+      <hr />
+      <div>{JSON.stringify(posts)}</div>
+    </div>
+  );
 };
 
 export default App;
--- src/store/counterStore.ts
+++ src/store/counterStore.ts
@@ -1,3 +1,45 @@
 import { create } from "zustand";
 
-export const useCounterStore = create(() => ({}));
+interface IPost {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface ICounterState {
+  count: number;
+  title: string;
+  increment: (value: number) => void;
+  posts: IPost[];
+  getPosts: () => Promise<void>;
+  clearStore: () => void;
+  multiply: (value: number) => void;
+}
+
+export const useCounterStore = create<ICounterState>((set, get) => ({
+  count: 10,
+  title: "Counter set to",
+  posts: [],
+  increment: (value: number) =>
+    set((state) => ({
+      count: state.count + value,
+    })),
+  getPosts: async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    console.log(data);
+
+    set((state) => ({
+      ...state,
+      posts: data,
+    }));
+  },
+  clearStore: () => {
+    set({}, true);
+  },
+
+  multiply: (value: number) => {
+    const { count } = get();
+    set({ count: count * value });
+  },
+}));
